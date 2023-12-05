b6c8bbf3044b000636e4a98924b67cf75ad8f521
Abdillahi485 <daakircadde143@gmail.com>
2023-12-05 06:17:25 +0300

    finished sum-two-smallest-numbers

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+function sumTwoSmallestNums(numbers) {  
+    let first = Math.min(...numbers)
+    numbers.splice(numbers.indexOf(first), 1)
+    let second = Math.min(...numbers)
+    return first + second
+  }
+
+
+console.log(sumTwoSmallestNums([12, 5, 24, 55, 18]) )
+console.log(sumTwoSmallestNums([190, 300, 230, 800]) )
